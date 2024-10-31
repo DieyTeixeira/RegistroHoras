685bf4180bee98a4c0a21b04f70fc8648a2fefef
DieyTeixeira <dieinison.teixeira@gmail.com>
2024-10-31 00:32:14 -0300

    RegistroHoras
    +

--- app/src/main/java/com/dieyteixeira/registrohoras/ui/components/RelatorioDialog.kt
+++ app/src/main/java/com/dieyteixeira/registrohoras/ui/components/RelatorioDialog.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,7 +28,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -35,6 +38,9 @@ import androidx.compose.ui.window.DialogProperties
 import com.dieyteixeira.registrohoras.R
 import com.dieyteixeira.registrohoras.model.Registro
 import com.dieyteixeira.registrohoras.ui.theme.Azul1
+import com.dieyteixeira.registrohoras.ui.theme.Azul2
+import com.dieyteixeira.registrohoras.ui.theme.Azul3
+import com.dieyteixeira.registrohoras.ui.theme.Azul4
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -82,26 +88,52 @@ fun RelatorioDialog(
                         .background(color = Azul1)
 
                 )
-                Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(5.dp))
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(5.dp)
                 ) {
                     items(listaRegistros) { registro ->
+                        Spacer(modifier = Modifier.height(2.dp))
                         Column {
                             // Formatação da data
                             val parsedDate =
                                 LocalDate.parse(registro.data) // assume que registro.data é "2024-10-30"
                             val formattedDate =
                                 parsedDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                            val formattedDayOfWeek =
+                                parsedDate.format(DateTimeFormatter.ofPattern("EEEE"))
 
-                            Text(text = "Data: $formattedDate")
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(
+                                        color = Azul4,
+                                        shape = RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp)
+                                    )
+                                    .padding(15.dp, 5.dp)
+                            ){
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        text = "Data:  ",
+                                        style = MaterialTheme.typography.titleLarge.copy(
+                                            fontSize = 17.sp
+                                        )
+                                    )
+                                    Text(
+                                        text = "$formattedDate  -  $formattedDayOfWeek",
+                                        style = MaterialTheme.typography.titleSmall.copy(
+                                            fontSize = 17.sp
+                                        )
+                                    )
+                                }
+                            }
 
                             // Função para formatar e exibir horas
                             @Composable
                             fun formatAndDisplayTime(
-                                label: String,
                                 initialMillis: Long,
                                 finalMillis: Long,
                             ) {
@@ -109,30 +141,81 @@ fun RelatorioDialog(
                                     val formattedStartTime = convertMillisToTime(initialMillis)
                                     val formattedEndTime = convertMillisToTime(finalMillis)
 
-                                    Text(text = "$label: Início: $formattedStartTime, Fim: $formattedEndTime")
+                                    Text(
+                                        text = "$formattedStartTime - $formattedEndTime",
+                                        style = MaterialTheme.typography.titleSmall.copy(
+                                            fontSize = 17.sp
+                                        )
+                                    )
                                 }
                             }
 
-                            formatAndDisplayTime(
-                                "Período 1",
-                                registro.initialMillisP1,
-                                registro.finalMillisP1
-                            )
-                            formatAndDisplayTime(
-                                "Período 2",
-                                registro.initialMillisP2,
-                                registro.finalMillisP2
-                            )
-                            formatAndDisplayTime(
-                                "Período 3",
-                                registro.initialMillisP3,
-                                registro.finalMillisP3
-                            )
-                            formatAndDisplayTime(
-                                "Período 4",
-                                registro.initialMillisP4,
-                                registro.finalMillisP4
-                            )
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(
+                                        color = Color.LightGray,
+                                        shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp)
+                                    )
+                                    .padding(15.dp, 5.dp)
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        text = "Horário:",
+                                        style = MaterialTheme.typography.titleLarge.copy(
+                                            fontSize = 17.sp
+                                        ),
+                                        modifier = Modifier.width(60.dp)
+                                    )
+                                    formatAndDisplayTime(
+                                        registro.initialMillisP1,
+                                        registro.finalMillisP1
+                                    )
+                                    if (registro.initialMillisP2 > 0) {
+                                        Text(
+                                            text = "  |  ",
+                                            style = MaterialTheme.typography.titleSmall.copy(
+                                                fontSize = 17.sp
+                                            )
+                                        )
+                                        formatAndDisplayTime(
+                                            registro.initialMillisP2,
+                                            registro.finalMillisP2
+                                        )
+                                    }
+                                }
+                                if (registro.initialMillisP3 > 0) {
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Text(
+                                            text = "Extra: ",
+                                            style = MaterialTheme.typography.titleLarge.copy(
+                                                fontSize = 17.sp
+                                            ),
+                                            modifier = Modifier.width(60.dp)
+                                        )
+                                        formatAndDisplayTime(
+                                            registro.initialMillisP3,
+                                            registro.finalMillisP3
+                                        )
+                                        if (registro.initialMillisP4 > 0) {
+                                            Text(
+                                                text = "  |  ",
+                                                style = MaterialTheme.typography.titleSmall.copy(
+                                                    fontSize = 17.sp
+                                                )
+                                            )
+                                            formatAndDisplayTime(
+                                                registro.initialMillisP4,
+                                                registro.finalMillisP4
+                                            )
+                                        }
+                                    }
+                                }
+                            }
 
                             Spacer(modifier = Modifier.height(8.dp))
                         }
@@ -173,4 +256,41 @@ fun convertMillisToTime(totalMillis: Long): String {
     val formattedTime = String.format("%02d:%02d", hours, minutes)
 
     return formattedTime
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+private fun RelatorioDialogPreview() {
+    val registrosSimulados = listOf(
+        Registro(
+            data = "2024-10-30",
+            initialMillisP1 = 8 * 60 * 60 * 1000L, // 8:00 AM em milissegundos
+            finalMillisP1 = 12 * 60 * 60 * 1000L,  // 12:00 PM
+            initialMillisP2 = 13 * 60 * 60 * 1000L, // 1:00 PM
+            finalMillisP2 = 17 * 60 * 60 * 1000L,  // 5:00 PM
+            initialMillisP3 = 0L,
+            finalMillisP3 = 0L,
+            initialMillisP4 = 0L,
+            finalMillisP4 = 0L
+        ),
+        Registro(
+            data = "2024-10-29",
+            initialMillisP1 = 9 * 60 * 60 * 1000L, // 9:00 AM
+            finalMillisP1 = 12 * 60 * 60 * 1000L,  // 12:00 PM
+            initialMillisP2 = 13 * 60 * 60 * 1000L, // 1:00 PM
+            finalMillisP2 = 18 * 60 * 60 * 1000L,  // 6:00 PM
+            initialMillisP3 = 18 * 60 * 60 * 1000L,
+            finalMillisP3 = 20 * 60 * 60 * 1000L,
+            initialMillisP4 = 0L,
+            finalMillisP4 = 0L
+        )
+    )
+
+    RelatorioDialog(
+        onDismissRequest = {},
+        onOKClick = {},
+        listaRegistros = registrosSimulados
+    )
+
 }
